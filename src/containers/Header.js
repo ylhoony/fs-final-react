244e1bb916c6a8cc388ad1cbe39c9a82cdad43d4
Hoon Lee <yl.hoony@gmail.com>
2018-07-05 19:31:09 -0700

    created Header file and set class with dummy contents

---
+++ src/containers/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <p>This is header</p>
+      </header>
+    )
+  }
+}
+
+export default Header;
