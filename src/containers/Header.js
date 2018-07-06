351da918bde9eb9a481982a83f5f5d789a056809
Hoon Lee <yl.hoony@gmail.com>
2018-07-06 14:48:29 -0700

    added structure for header navbar

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <p>This is header</p>
+        <nav className="uk-navbar-container uk-navbar">
+          <div className="uk-navbar-left">
+            <ul className="uk-navbar-nav">
+              <li>
+                <a
+                  className="uk-navbar-toggle uk-navbar-toggle-icon"
+                  href="#"
+                />
+              </li>
+              <li>
+                <Link to="/" className="uk-text-muted">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/dashboard" className="uk-text-muted">
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="uk-navbar-right">
+            <ul className="uk-navbar-nav">
+              <li><a>Sign out</a></li>
+            </ul>
+          </div>
+        </nav>
       </header>
-    )
+    );
   }
 }
 
