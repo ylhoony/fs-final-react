790f0b3bd0bd56df2c0f80f148d2cb99531f0216
Hoon Lee <yl.hoony@gmail.com>
2018-07-05 19:57:22 -0700

    added/set router for signin and signup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './containers/Header';
 import SignUpPage from './containers/SignUpPage';
+import SignInPage from './containers/SignInPage';
 
 // import logo from './logo.svg';
 import './App.css';
 
-
-
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <SignUpPage />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/signup" component={SignUpPage} />
+          <Route exact path="/signin" component={SignInPage} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
