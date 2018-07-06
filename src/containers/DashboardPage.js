2d00ce4b8d512bbad09440f33277e6e10cab02eb
Hoon Lee <yl.hoony@gmail.com>
2018-07-06 14:50:01 -0700

    created dashboard page with dummy contents

---
+++ src/containers/DashboardPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DashboardPage extends Component {
+  render () {
+    return (
+      <h2>This is Dashboard Page.</h2>
+    )
+  }
+}
+
+export default DashboardPage;
