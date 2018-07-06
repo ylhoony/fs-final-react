f3c77c028e28900f2fa24d0434e31e41e6cfee60
Hoon Lee <yl.hoony@gmail.com>
2018-07-06 14:49:43 -0700

    added routes for homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './containers/Header';
+import HomePage from './containers/HomePage';
 import SignUpPage from './containers/SignUpPage';
 import SignInPage from './containers/SignInPage';
+import DashboardPage from './containers/DashboardPage';
 
 // import logo from './logo.svg';
 import './App.css';
@@ -13,8 +15,10 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Header />
-          <Route exact path="/signup" component={SignUpPage} />
-          <Route exact path="/signin" component={SignInPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/signup' component={SignUpPage} />
+          <Route exact path='/signin' component={SignInPage} />
+          <Route exact path='/dashboard' component={DashboardPage} />
         </div>
       </BrowserRouter>
     )
