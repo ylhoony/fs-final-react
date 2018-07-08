61ca715201eceeaa33a22db060ce969f25e3b18f
Hoon Lee <yl.hoony@gmail.com>
2018-07-08 14:32:26 -0700

    connected to redux thunk, and set default auth logic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { actions } from './actions/index';
+
 import Header from './containers/Header';
-import HomePage from './containers/HomePage';
-import SignUpPage from './containers/SignUpPage';
-import SignInPage from './containers/SignInPage';
+import SignUpPage from './containers/users/SignUpPage';
+import SignInPage from './containers/users/SignInPage';
 import DashboardPage from './containers/DashboardPage';
 
 // import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.actions.authUser();
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/signup' component={SignUpPage} />
-          <Route exact path='/signin' component={SignInPage} />
-          <Route exact path='/dashboard' component={DashboardPage} />
-        </div>
-      </BrowserRouter>
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            { this.props.user && <Header /> }
+            
+            <Route exact path='/signup' component={SignUpPage} />
+            <Route exact path='/signin' component={SignInPage} />
+            <Route exact path='/dashboard' component={DashboardPage} />
+          </div>
+        </BrowserRouter>
+      </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps({ user }) {  
+  return { user };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(actions, dispatch) }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
