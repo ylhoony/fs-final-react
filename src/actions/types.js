930431cf44132c67474eacc003a7f948278323ee
Hoon Lee <yl.hoony@gmail.com>
2018-07-12 14:46:54 -0700

    added CREATE_COMPNAY action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,8 +1,11 @@
 const types = {
+  // auth user
   AUTH_USER: 'AUTH_USER',
   SIGN_UP_USER: 'SIGN_UP_USER',
   SIGN_IN_USER: 'SIGN_IN_USER',
   SIGN_OUT_USER: 'SIGN_OUT_USER',
+  // companies
+  CREATE_COMPANAY: 'CREATE_COMPANAY',
 }
 
 export default types;
