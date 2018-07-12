8d7df5664288d3d802022ee1057af5434a906f2b
Hoon Lee <yl.hoony@gmail.com>
2018-07-12 14:45:47 -0700

    set default logic to companies reducer

---
+++ src/reducers/companiesReducer.js
@@ -0,0 +1,10 @@
+import types from '../actions/types';
+
+export const companiesReducer = (state = {}, action) => {
+  // console.log(action);
+  switch(action.type) {
+
+    default: 
+      return state;
+  }
+}
