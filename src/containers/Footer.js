9fc622db0adc7225eac88f7349eeec8f14ef116f
Hoon Lee <yl.hoony@gmail.com>
2018-07-12 14:51:57 -0700

    created footer container

---
+++ src/containers/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        this is footer.
+      </footer>
+    )
+  }
+}
+
+export default Footer;
