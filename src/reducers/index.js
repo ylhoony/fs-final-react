635288659e30a053b294c85b7da164b63ce71bb9
Hoon Lee <yl.hoony@gmail.com>
2018-07-12 14:55:52 -0700

    added companies to combined reducer list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+
+import { companiesReducer } from './companiesReducer';
 import { countriesReducer } from './countriesReducer';
 import { userReducer } from './userReducer';
 
 export const rootReducer = combineReducers({
+  companies: companiesReducer,
   countries: countriesReducer,
   user: userReducer
 })
