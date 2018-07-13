369cd267b624883ec85714378781a5a986c59812
Hoon Lee <yl.hoony@gmail.com>
2018-07-13 14:03:14 -0700

    refactored with async and await

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -5,21 +5,15 @@ export const authUser = () => async dispatch => {
   const res = await axios.get('/api/v1/current_user', {
     headers: {
       'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }})
-  dispatch({ type: types.AUTH_USER, payload: res.data })
+    }});
+  dispatch({ type: types.AUTH_USER, payload: res.data });
 };
 
-export const signUp = (data) => {
-  return function(dispatch) {
-    axios
-      .post('/api/v1/sign_up', data)
-      .then((res) => {
-        debugger;
-        localStorage.setItem('token', res.data.token)
-        dispatch({ type: types.SIGN_UP_USER, payload: res.data.user })
-      })
-  }
-};
+export const signUp = (data) => async dispatch => {
+  const res = await axios.post('/api/v1/sign_up', data);
+  localStorage.setItem('token', res.data.token);
+  dispatch({ type: types.SIGN_UP_USER, payload: res.data.user });
+}
 
 export const signIn = (data) => async dispatch => {
   const res = await axios.post('/api/v1/sign_in', data);
@@ -28,9 +22,7 @@ export const signIn = (data) => async dispatch => {
 }
 
 
-export const signOut = () => {
-  return function(dispatch) {
-    localStorage.removeItem('token');
-    dispatch({ type: types.SIGN_OUT_USER, payload: null });
-  }
-};
+export const signOut = () => dispatch => {
+  localStorage.removeItem('token');
+  dispatch({ type: types.SIGN_OUT_USER, payload: null });
+}
