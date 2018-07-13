9536ff31d16fd0bb845c9f170e134f44a086e470
Hoon Lee <yl.hoony@gmail.com>
2018-07-13 13:30:29 -0700

    refactored form with semantic-ui

--- src/containers/users/SignIn.js
+++ src/containers/users/SignIn.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Button, Form, Header, List, Segment } from 'semantic-ui-react'
+
 import { actions } from '../../actions/index';
 
 class SignIn extends Component {
@@ -26,43 +28,58 @@ class SignIn extends Component {
 
   handleSignIn = async e => {
     e.preventDefault();
-    await this.props.actions.signIn(this.state)
+    const token = localStorage.getItem('token');
+    if (token) {
+      localStorage.removeItem('token');
+    }
+  
+    const res = await this.props.actions.signIn(this.state);
     this.props.history.push('/dashboard');
   }
 
   render() {
     return (
+      
       <React.Fragment>
-        <main className="uk-card">
-          <p>Sign in</p>
-
-          <form onSubmit={(event) => this.handleSignIn(event)}>
-            <div>
-              <label htmlFor='email'>Email</label>
-              <input
-                id='email'
-                name='email'
-                required
-                type='email'
-                value={this.state.email}
-                onChange={(event) => this.handleInputChange(event)}
-              />
-            </div>
-            <div>
-              <label htmlFor='password'>Password</label>
-              <input
-                id='password'
-                name='password'
-                required
-                type='password'
-                value={this.state.password}
-                onChange={(event) => this.handleInputChange(event)}
-              />
-            </div>
-            <button type="submit">Sign in</button>
-          </form>
-          <p><Link to='/signup'>Sign up</Link></p>
-        </main>
+        {/* <section className="sign-in flex flex-center flex-center-vertical vh-100"> */}
+        <div className="sign-in flex flex-center flex-center-vertical vh-100">
+        <Segment padded>
+          <Form onSubmit={(event) => this.handleSignIn(event)}>
+            <header className="padding-bottom-lg">
+              <Header as='h2'>Sign in</Header>
+            </header>
+            <main className="padding-all-sm">
+              <Form.Field>
+                <label htmlFor="email">Email</label>
+                <input
+                  id='email'
+                  name='email'
+                  required
+                  type='email'
+                  value={this.state.email}
+                  onChange={(event) => this.handleInputChange(event)}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label htmlFor='password'>Password</label>
+                <input
+                  id='password'
+                  name='password'
+                  required
+                  type='password'
+                  value={this.state.password}
+                  onChange={(event) => this.handleInputChange(event)}
+                />
+              </Form.Field>
+            </main>
+            <footer className="flex flex-space-between flex-center-vertical padding-all-md">
+              <List.Item href="/signup">Sign up</List.Item>
+              <Button type="submit" color="green" size="small">Sign in</Button>
+            </footer>
+          </Form>
+        </Segment>
+        </div>
+        {/* </section> */}
       </React.Fragment>
     )
   }
