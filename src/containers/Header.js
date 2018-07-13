2ab110b77f59e20f1f5723f1a61d83a9a4037903
Hoon Lee <yl.hoony@gmail.com>
2018-07-13 15:55:01 -0700

    updated header navigation with semantic-ui

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Dropdown, Icon, Input, Menu, Segment } from 'semantic-ui-react'
+
 import { actions } from '../actions/index';
 
 class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      activeItem: window.location.pathname,
+      companySelected: 'Company Name Inc.'
+    }
+  }
+
+
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+  }
 
   handleSignOut = (e) => {
     e.preventDefault();
@@ -14,60 +28,103 @@ class Header extends Component {
 
   render() {
     return (
-      <header>
-        <nav className='uk-navbar-container uk-navbar'>
-          <div className='uk-navbar-left'>
-            <ul className='uk-navbar-nav'>
-              <li>
-                <Link to='/' className='uk-text-muted'>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to='/dashboard' className='uk-text-muted'>
-                  Dashboard
-                </Link>
-              </li>
-              <li>
-                <Link to='/demand' className='uk-text-muted'>
-                  Demand
-                </Link>
-              </li>
-              <li>
-                <Link to='/supply' className='uk-text-muted'>
-                  Supply
-                </Link>
-              </li>
-              <li>
-                <Link to='/product' className='uk-text-muted'>
-                  Product
-                </Link>
-              </li>
-              <li>
-                <Link to='/logistics' className='uk-text-muted'>
-                  Logistics
-                </Link>
-              </li>
-              <li>
-                <Link to='/warehouse' className='uk-text-muted'>
-                  Warhouse
-                </Link>
-              </li>
-              <li>
-                <Link to='/setting' className='uk-text-muted'>
-                  Setting
-                </Link>
-              </li>
-            </ul>
-          </div>
+      <React.Fragment>
+        <header>
+          <Menu pointing className='margin-remove'>
+            <Menu.Item>
+              <Icon name='bars' />
+            </Menu.Item>
+            <Menu.Menu>
+              <Dropdown item text={this.state.companySelected}>
+                <Dropdown.Menu>
+                  <Dropdown.Item>English</Dropdown.Item>
+                  <Dropdown.Item>Russian</Dropdown.Item>
+                  <Dropdown.Item>Spanish</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Menu>
+            <Menu.Menu position='right'>                  
+              <div className='ui right aligned category search item'>
+                <div className='ui transparent icon input'>
+                  <input className='prompt' type='text' placeholder='Search...' />
+                  <i className='search link icon' />
+                </div>
+                <div className='results' />
+              </div>
 
-          <div className='uk-navbar-right'>
-            <ul className='uk-navbar-nav'>
-              <li><a href='/sign_out' onClick={(event) => this.handleSignOut(event)}>Sign out</a></li>
-            </ul>
-          </div>
-        </nav>
-      </header>
+              <Dropdown item icon='user' simple>
+                <Dropdown.Menu>
+                  <Dropdown.Item>
+                    <Icon name='dropdown' />
+                    <span className='text'>New</span>
+
+                    <Dropdown.Menu>
+                      <Dropdown.Item>Document</Dropdown.Item>
+                      <Dropdown.Item>Image</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown.Item>
+                  <Dropdown.Item>Open</Dropdown.Item>
+                  <Dropdown.Item>Save...</Dropdown.Item>
+                  <Dropdown.Item>Edit Permissions</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Export</Dropdown.Header>
+                  <Dropdown.Item>Share</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Menu.Item onClick={(event) => this.handleSignOut(event)}>Sign out</Menu.Item>
+            </Menu.Menu>
+          </Menu>
+
+          <Menu pointing secondary className='margin-remove'>
+            <Menu.Item
+              href='/dashboard'
+              name='dashboard'
+              active={this.state.activeItem === '/dashboard'}
+              onClick={this.handleItemClick} 
+            />
+            <Menu.Item
+              href='/demand'
+              name='demand'
+              active={this.state.activeItem === '/demand'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              href='/supply'
+              name='supply'
+              active={this.state.activeItem === '/supply'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              href='/product'
+              name='product'
+              active={this.state.activeItem === '/product'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              href='/logistics'
+              name='logistics'
+              active={this.state.activeItem === '/logistics'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              href='/warehouse'
+              name='warehouse'
+              active={this.state.activeItem === '/warehouse'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              href='/setting'
+              name='setting'
+              active={this.state.activeItem === '/setting'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Menu position='right'>
+              <Menu.Item onClick={(event) => this.handleSignOut(event)}>Sign out</Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </header>
+      </React.Fragment>
     );
   }
 }
