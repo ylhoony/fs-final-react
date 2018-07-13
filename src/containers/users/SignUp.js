24ddd564793b37f6badc162a3d0ae1bca5c33815
Hoon Lee <yl.hoony@gmail.com>
2018-07-13 13:54:38 -0700

    updated form with semantic-ui

--- src/containers/users/SignUp.js
+++ src/containers/users/SignUp.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Button, Form, Header, List, Segment } from 'semantic-ui-react';
+
 import { actions } from '../../actions/index';
 
 class SignUp extends Component {
@@ -36,66 +38,71 @@ class SignUp extends Component {
   render() {
     return (
       <React.Fragment>
-      <main className='uk-height-viewport uk-flex uk-flex-center uk-flex-middle'>
-        <section>
-          <p>Sign up</p>
-
-          <form onSubmit={(event) => this.handleFormSubmit(event)}>
-            <div>
-              <label htmlFor='first_name'>First Name</label>
-              <input 
-                id='first_name'
-                name='first_name'
-                required
-                type='text'
-                onChange={(event) => this.handleInputChange(event)}
-              />
-            </div>
-            <div>
-              <label htmlFor='last_name'>Last Name</label>
-              <input
-                id='last_name'
-                name='last_name'
-                required
-                type='text'
-                onChange={(event) => this.handleInputChange(event)}
-              />
-            </div>
-            <div>
-              <label htmlFor='email'>Email</label>
-              <input
-                id='email'
-                name='email'
-                required
-                type='email'
-                onChange={(event) => this.handleInputChange(event)}
-              />
-            </div>
-            <div>
-              <label htmlFor='password'>Password</label>
-              <input
-                id='password'
-                name='password'
-                required
-                type='password'
-                onChange={(event) => this.handleInputChange(event)}
-              />
-            </div>
-            <div>
-              <label htmlFor='password_confirmation'>Password Confirmation</label>
-              <input
-                id='password_confirmation'
-                name='password_confirmation'
-                required
-                type='password'
-                onChange={(event) => this.handleInputChange(event)}
-              />
-            </div>
-            <button type="submit">Sign up</button>
-          </form>
-          <p><Link to='/signin'>Sign in</Link></p>
+        <section className="container-auth flex flex-center flex-middle vh-100">
+          <Segment padded>
+            <Form onSubmit={(event) => this.handleFormSubmit(event)}>
+            <header className="padding-bottom-lg">
+                <Header as='h2'>Sign up</Header>
+              </header>
+              <main className="padding-all-sm">
+                <Form.Field>
+                  <label htmlFor='first_name'>First Name</label>
+                  <input 
+                    id='first_name'
+                    name='first_name'
+                    required
+                    type='text'
+                    onChange={(event) => this.handleInputChange(event)}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label htmlFor='last_name'>Last Name</label>
+                  <input
+                    id='last_name'
+                    name='last_name'
+                    required
+                    type='text'
+                    onChange={(event) => this.handleInputChange(event)}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label htmlFor='email'>Email</label>
+                  <input
+                    id='email'
+                    name='email'
+                    required
+                    type='email'
+                    onChange={(event) => this.handleInputChange(event)}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label htmlFor='password'>Password</label>
+                  <input
+                    id='password'
+                    name='password'
+                    required
+                    type='password'
+                    onChange={(event) => this.handleInputChange(event)}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label htmlFor='password_confirmation'>Password Confirmation</label>
+                  <input
+                    id='password_confirmation'
+                    name='password_confirmation'
+                    required
+                    type='password'
+                    onChange={(event) => this.handleInputChange(event)}
+                  />
+                </Form.Field>
+              </main>
+              <footer className="flex flex-between flex-middle padding-all-md">
+                <List.Item href="/signin">Sign in</List.Item>
+                <Button type="submit" color="green" size="small">Sign up</Button>
+              </footer>
+            </Form>
+          </Segment>
         </section>
-      </main>
       </React.Fragment>
     )
   }
