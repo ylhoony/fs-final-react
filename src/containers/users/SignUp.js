2d7a64285dae3604fe9ab42a57294e6e8613cbc7
Hoon Lee <yl.hoony@gmail.com>
2018-07-13 22:45:40 -0700

    removed unused line

--- src/containers/users/SignUp.js
+++ src/containers/users/SignUp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Button, Form, Header, List, Segment } from 'semantic-ui-react';
@@ -7,8 +7,8 @@ import { Button, Form, Header, List, Segment } from 'semantic-ui-react';
 import { actions } from '../../actions/index';
 
 class SignUp extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       user: {
