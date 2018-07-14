93e250baf7c5afee519a340320046e7a4fec90d2
Hoon Lee <yl.hoony@gmail.com>
2018-07-13 22:36:34 -0700

    moved Routes from Main file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,68 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { actions } from './actions/index';
 
-import Header from './containers/Header';
-import Main from './containers/Main';
-import Footer from './containers/Footer';
-
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+import Header from './containers/Header';
+import Footer from './containers/Footer';
 
+import SignUp from './containers/users/SignUp';
+import SignIn from './containers/users/SignIn';
+
+import AuthenticatedRoutes from './containers/AuthenticatedRoutes';
+// Navigation Containers
+import Dashboard from './containers/navs/Dashboard';
+import Demand from './containers/navs/Demand';
+import Supply from './containers/navs/Supply';
+import Product from './containers/navs/Product';
+import Logistics from './containers/navs/Logistics';
+import Warehouse from './containers/navs/Warehouse';
+import Setting from './containers/navs/Setting';
+// Menu Containers
+import AccountsPage from './containers/accounts/AccountsPage'
+
+class App extends Component {
   render() {
-    return (
-      <div className="container">
-        <BrowserRouter>
-          <div>
-            { this.props.user && <Header /> }
-            <Main />
-            { this.props.user && <Footer /> }
-          </div>
-        </BrowserRouter>
-      </div>
+    return(
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/signin' component={SignIn} />
+          <AuthenticatedRoutes>
+            <Header />
+            <main className="padding-all-md">
+              <Route exact path='/' component={Dashboard}/>
+              <Route exact path='/dashboard' component={Dashboard} />
+              <Route exact path='/demand' component={Demand} />
+              <Route exact path='/supply' component={Supply} />
+              <Route exact path='/product' component={Product} />
+              <Route exact path='/logistics' component={Logistics} />
+              <Route exact path='/warehouse' component={Warehouse} />
+              <Route exact path='/setting' component={Setting} />
+
+              <Route path='/accounts' component={AccountsPage} />
+            </main>
+            <Footer />
+          </AuthenticatedRoutes>
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
 
-function mapStateToProps({ user }) {
+function mapStateToProps({ user }) {  
   return { user };
 }
 
-function mapDispatchToProps(dispatch) {
-  return { actions: bindActionCreators(actions, dispatch) }
+function mapDispatchToProps(dispatch) {  
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
