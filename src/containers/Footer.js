9cb49685986339e47e5df7edcaea75552109a32c
Hoon Lee <yl.hoony@gmail.com>
2018-07-16 16:27:44 -0700

    updated the footer with segment.

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Segment } from '../../node_modules/semantic-ui-react';
 
 class Footer extends Component {
   render() {
     return (
       <footer>
-        this is footer.
+        <Segment>
+          <p>this is footer.</p>
+        </Segment>
       </footer>
     )
   }
