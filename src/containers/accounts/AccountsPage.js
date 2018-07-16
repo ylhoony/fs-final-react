94019cd448e66ae495697a0fffe5d725326f5a66
Hoon Lee <yl.hoony@gmail.com>
2018-07-16 16:32:14 -0700

    refactored by moving contents to route pages

--- src/containers/accounts/AccountsPage.js
+++ src/containers/accounts/AccountsPage.js
@@ -1,39 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Segment } from 'semantic-ui-react';
+
 import { actions } from '../../actions/index';
 
 import AccountsList from '../../components/accounts/AccountsList';
+import AccountNew from '../../components/accounts/AccountNew';
+import AccountShow from '../../components/accounts/AccountShow';
+import Loading from '../../components/Loading';
 
-
-class AccountsPage extends Component {
-
+class AccountsPage extends React.Component {
   componentDidMount() {
-    console.log(this.props.actions);
     this.props.actions.getAccounts();
   }
 
   render() {
+    const { match, error, loading, accounts } = this.props;    
+
+    if (loading) {
+      return <Loading />
+    }
+
     return (
-      <div>
-        <p>Accounts Page</p>
-        <AccountsList accounts={this.props.accounts} />
-        
-
-        {/* <MoviesList movies={movies} />
-        <Route path={`${match.url}/:movieId`} component={MovieShow}/>
-        <Route exact path={match.url} render={() => (
-          <h3>Please select a Movie from the list.</h3>
-        )}/> */}
-
-      </div>
+      <React.Fragment>
+        <main>
+          <Segment.Group>
+            <Route exact path={`${match.url}`} component={() => <AccountsList accounts={accounts} />} />
+            <Route exact path={`${match.url}/new`} component={AccountNew} />
+            {/* <Route path={`${match.url}/:movieId`} component={AccountShow} /> */}
+          </Segment.Group>
+        </main>
+      </React.Fragment>
     )
   }
 }
 
 function mapStateToProps({ accounts }) {  
-  return { accounts };
+  return { 
+    accounts: accounts.accounts,
+    loading: accounts.loading,
+    error: accounts.error
+  };
 }
 
 function mapDispatchToProps(dispatch) {  
