eba96c215fc785ce8c2481e2fdf9026d90fd6a33
Hoon Lee <yl.hoony@gmail.com>
2018-07-16 16:26:52 -0700

    added GET_ACCOUNTS_BEGIN, AND GET_ACCOUNTS_SUCCESS

--- src/reducers/accountsReducer.js
+++ src/reducers/accountsReducer.js
@@ -1,9 +1,26 @@
 import types from '../actions/types';
 
-export const accountsReducer = (state = [], action) => {
+const initialState = {
+  accounts: [],
+  loading: true,
+  error: null
+}
+
+export const accountsReducer = (state = initialState, action) => {
   switch(action.type) {
-    case types.GET_ACCOUNTS:
-      return action.payload
+    case types.GET_ACCOUNTS_BEGIN:
+    return {
+      ...state,
+      loading: true,
+      error: null
+    }
+
+    case types.GET_ACCOUNTS_SUCCESS:
+      return {
+        ...state,
+        accounts: action.payload,
+        loading: false
+      }
 
     default: 
       return state;
