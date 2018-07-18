bf7d53460bac40fdc9c393394e6ee908b7854cac
Hoon Lee <yl.hoony@gmail.com>
2018-07-17 19:47:14 -0700

    updated structure with dummy contents

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Segment } from '../../node_modules/semantic-ui-react';
+import { List, Segment } from '../../node_modules/semantic-ui-react';
 
 class Footer extends Component {
   render() {
     return (
       <footer>
         <Segment>
-          <p>this is footer.</p>
+          <List className='flex flex-around'>
+            <List.Item>Apples</List.Item>
+            <List.Item>Pears</List.Item>
+            <List.Item>Oranges</List.Item>
+          </List>
         </Segment>
       </footer>
     )
