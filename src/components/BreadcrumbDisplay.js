50508459ee88bdac329cca5e32617f594f94ab80
Hoon Lee <yl.hoony@gmail.com>
2018-07-17 18:02:32 -0700

    formatted with eslint

--- src/components/BreadcrumbDisplay.js
+++ src/components/BreadcrumbDisplay.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import { Breadcrumb, Segment } from 'semantic-ui-react';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Breadcrumb, Segment } from "semantic-ui-react";
 
 const BreadcrumbDisplay = ({ breadcrumbList }) => {
   const breadcrumbBuilder = breadcrumbList.map((e, index) => {
     if (index === breadcrumbList.length - 1) {
       return (
         <React.Fragment key={index}>
-          <Breadcrumb.Divider icon='right chevron' />
+          <Breadcrumb.Divider icon="right chevron" />
           <Breadcrumb.Section active>{e}</Breadcrumb.Section>
         </React.Fragment>
-      )
+      );
     } else {
       return (
         <React.Fragment key={index}>
-          <Breadcrumb.Divider icon='right chevron' />        
-          <Breadcrumb.Section link key={index}>{e}</Breadcrumb.Section>
+          <Breadcrumb.Divider icon="right chevron" />
+          <Breadcrumb.Section link key={index}>
+            {e}
+          </Breadcrumb.Section>
         </React.Fragment>
-      )
+      );
     }
-  })
+  });
 
   return (
     <Segment>
-      <Breadcrumb size='small'>
+      <Breadcrumb size="small">
         <Breadcrumb.Section link>Home</Breadcrumb.Section>
-        { breadcrumbBuilder }
+        {breadcrumbBuilder}
       </Breadcrumb>
     </Segment>
-  )
-}
+  );
+};
 
-export default BreadcrumbDisplay;
+export default withRouter(BreadcrumbDisplay);
