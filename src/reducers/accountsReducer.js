b22942e81eca64ef8abfcdf7022538015e8db5c5
Hoon Lee <yl.hoony@gmail.com>
2018-07-17 19:45:29 -0700

    updated state names

--- src/reducers/accountsReducer.js
+++ src/reducers/accountsReducer.js
@@ -2,24 +2,39 @@ import types from '../actions/types';
 
 const initialState = {
   accounts: [],
-  loading: true,
-  error: null
+  accountsLoading: true,
+  accountsError: null,
+  selectedAccount: {},
 }
 
 export const accountsReducer = (state = initialState, action) => {
+  // console.log("Accounts Reducer", action);
   switch(action.type) {
     case types.GET_ACCOUNTS_BEGIN:
     return {
       ...state,
-      loading: true,
-      error: null
+      accountsLoading: true,
+      accountsError: null,
     }
 
     case types.GET_ACCOUNTS_SUCCESS:
       return {
         ...state,
         accounts: action.payload,
-        loading: false
+        accountsLoading: false
+      }
+
+    case types.GET_ACCOUNTS_FAILURE:
+      return {
+        ...state,
+        accountsLoading: false,
+        accountsError: true
+      }
+
+    case types.CREATE_ACCOUNT:
+      return {
+        ...state,
+        selectedAccount: action.payload
       }
 
     default: 
