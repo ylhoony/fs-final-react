a040f3daba8365d15686269c174754d3bf533fdb
Hoon Lee <yl.hoony@gmail.com>
2018-07-17 19:46:26 -0700

    set logic for GET_COUNTRIES process

--- src/reducers/countriesReducer.js
+++ src/reducers/countriesReducer.js
@@ -1,8 +1,34 @@
 import types from '../actions/types';
 
-export const countriesReducer = (state = {}, action) => {
-  // console.log(action);
+const initialState = {
+  countries: [],
+  countriesLoading: true,
+  countriesError: null,
+}
+
+export const countriesReducer = (state = initialState, action) => {
+  // console.log("Countries Reducer", action)
   switch(action.type) {
+    case 'GET_COUNTRIES_BEGIN':
+      return {
+        ...state,
+        countriesLoading: true,
+        countriesError: null
+      }
+
+    case 'GET_COUNTRIES_SUCCESS':
+      return {
+        ...state,
+        countries: action.payload,
+        countriesLoading: false,
+      }
+
+    case 'GET_COUNTRIES_FAILURE':
+      return {
+        ...state,
+        countriesLoading: false,
+        countriesError: true
+      }
 
     default: 
       return state;
