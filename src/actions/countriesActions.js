682bf97d918c1fb39631b1efcc9b881fb228caa4
Hoon Lee <yl.hoony@gmail.com>
2018-07-17 19:49:18 -0700

    created file and set getCountries

---
+++ src/actions/countriesActions.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const getCountries = () => async dispatch => {
+  try {
+    dispatch({ type: 'GET_COUNTRIES_BEGIN' })
+    const res = await axios.get('/api/v1/countries', {
+      headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+    });
+    dispatch({ type: 'GET_COUNTRIES_SUCCESS', payload: res.data })
+  } catch (err) {
+    dispatch({ type: 'GET_COUNTRIES_FAILURE', payload: err })
+  }
+};
