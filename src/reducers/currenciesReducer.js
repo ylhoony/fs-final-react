8a91793341b55ff33e4248d2339db0430f878b4e
Hoon Lee <yl.hoony@gmail.com>
2018-07-17 19:39:58 -0700

    created currenciesReducer and added GET_CURRENCIES process

---
+++ src/reducers/currenciesReducer.js
@@ -0,0 +1,36 @@
+import types from '../actions/types';
+
+const initialState = {
+  currencies: [],
+  currenciesLoading: true,
+  currenciesError: null,
+}
+
+export const currenciesReducer = (state = initialState, action) => {
+  // console.log("Currencies Reducer", action)
+  switch(action.type) {
+    case 'GET_CURRENCIES_BEGIN':
+      return {
+        ...state,
+        currenciesLoading: true,
+        currenciesError: null
+      }
+
+    case 'GET_CURRENCIES_SUCCESS':
+      return {
+        ...state,
+        currencies: action.payload,
+        currenciesLoading: false,
+      }
+
+    case 'GET_CURRENCIES_FAILURE':
+      return {
+        ...state,
+        currenciesLoading: false,
+        currenciesError: true
+      }
+
+    default: 
+      return state;
+  }
+}
