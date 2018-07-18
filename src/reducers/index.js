317d855b116a5c2af47442673a535ea520bc139f
Hoon Lee <yl.hoony@gmail.com>
2018-07-17 19:56:29 -0700

    added currenciesReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import { accountsReducer } from './accountsReducer';
 import { countriesReducer } from './countriesReducer';
+import { currenciesReducer } from './currenciesReducer';
 import { userReducer } from './userReducer';
 
 export const rootReducer = combineReducers({
   accounts: accountsReducer,
   countries: countriesReducer,
+  currencies: currenciesReducer,
   user: userReducer
 })
