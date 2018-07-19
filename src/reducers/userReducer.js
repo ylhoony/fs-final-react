a92835765495244fe9592be7b9fd7d8e9d67d27f
Hoon Lee <yl.hoony@gmail.com>
2018-07-18 18:05:50 -0700

    set initalState, and added change_account process

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,18 +1,59 @@
 import types from '../actions/types';
 
-export const userReducer = (state = null, action) => {
+const initialState = {
+  currentAccount: null,
+  currentAccountLoading: false,
+  currentAccountError: null,
+
+  currentUser: null
+}
+
+export const userReducer = (state = initialState, action) => {
   switch(action.type) {
     case types.AUTH_USER:
-      return action.payload || false;
+      return {
+        ...state,
+        currentUser: action.payload || false
+      }
 
     case types.SIGN_UP_USER:
-      return action.payload;
+      return {
+        ...state,
+        currentUser: action.payload
+      }
 
     case types.SIGN_IN_USER:
-      return action.payload;
+      return {
+        ...state,
+        currentUser: action.payload
+      }
 
     case types.SIGN_OUT_USER:
-      return action.payload;
+      return {
+        ...state,
+        currentUser: action.payload
+      }
+
+    case 'CHANGE_CURRENT_ACCOUNT_BEGIN':
+      return {
+        ...state,
+        currentAccountLoading: true
+      }
+
+    case 'CHANGE_CURRENT_ACCOUNT_SUCCESS':
+      return {
+        ...state,
+        currentAccountLoading: false,
+        currentAccount: action.payload
+      }
+
+    case 'CHANGE_CURRENT_ACCOUNT_FAILURE':
+    return {
+      ...state,
+      currentAccountLoading: false,
+      currentAccountError: true
+    }
+    
 
     default:
       return state;
