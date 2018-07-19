5b90c1408163dd42242b387e64fe9d7f8fe9964c
Hoon Lee <yl.hoony@gmail.com>
2018-07-18 18:11:29 -0700

    reformatted eslint

--- src/actions/accountsActions.js
+++ src/actions/accountsActions.js
@@ -1,38 +1,36 @@
-import axios from 'axios';
-import types from './types';
+import axios from "axios";
+import types from "./types";
 
 export const getAccounts = () => async dispatch => {
-  dispatch({ type: types.GET_ACCOUNTS_BEGIN })
+  dispatch({ type: types.GET_ACCOUNTS_BEGIN });
   try {
-    const res = await fetch('/api/v1/accounts', {
-      method: 'GET',
+    const res = await fetch("/api/v1/accounts", {
+      method: "GET",
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json'
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json"
       }
-    })
+    });
     const accountsList = await res.json();
-    dispatch({ type: types.GET_ACCOUNTS_SUCCESS, payload: accountsList })
-  } catch(err) {
-    dispatch({ type: types.GET_ACCOUNTS_FAILURE, payload: err })
+    dispatch({ type: types.GET_ACCOUNTS_SUCCESS, payload: accountsList });
+  } catch (err) {
+    dispatch({ type: types.GET_ACCOUNTS_FAILURE, payload: err });
   }
-}
+};
 
-export const createAccount = (data) => async dispatch => {
+export const createAccount = data => async dispatch => {
   try {
     const res = await axios({
-      method: 'post',
-      url: '/api/v1/accounts',
+      method: "post",
+      url: "/api/v1/accounts",
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json"
       },
       data: data
-    })
-    console.log(res);
+    });
     dispatch({ type: types.CREATE_ACCOUNT, payload: res.data });
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
-}
-
+};
