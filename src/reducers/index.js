76aefc9eb3d72596f7e5988d7188d152d8d8ac8e
Hoon Lee <yl.hoony@gmail.com>
2018-07-19 11:02:31 -0700

    added paymentOptions  reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import { accountsReducer } from './accountsReducer';
 import { countriesReducer } from './countriesReducer';
 import { currenciesReducer } from './currenciesReducer';
+import { paymentOptionsReducer } from './paymentOptionsReducer';
 import { userReducer } from './userReducer';
 
 export const rootReducer = combineReducers({
   accounts: accountsReducer,
   countries: countriesReducer,
   currencies: currenciesReducer,
-  user: userReducer
+  paymentOptions: paymentOptionsReducer,
+  user: userReducer,
 })
