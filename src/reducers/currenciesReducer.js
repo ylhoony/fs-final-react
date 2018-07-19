411f723ad83d7311e66e86b7cac7332477c02119
Hoon Lee <yl.hoony@gmail.com>
2018-07-19 01:48:32 -0700

    updated default currenciesLoading state to false

--- src/reducers/currenciesReducer.js
+++ src/reducers/currenciesReducer.js
@@ -1,8 +1,8 @@
-import types from '../actions/types';
+// import types from '../actions/types';
 
 const initialState = {
   currencies: [],
-  currenciesLoading: true,
+  currenciesLoading: false,
   currenciesError: null,
 }
 
