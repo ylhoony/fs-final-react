f8ccc7d5a038915d22043d4357603cebecf244e8
Hoon Lee <yl.hoony@gmail.com>
2018-07-19 11:00:22 -0700

    removed testing lines

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,7 +1,3 @@
-// export function getAuthToken() {
-//   return `Bearer ${localStorage.getItem("token")}`
-// }
-
 export const getAuthToken = () => {
   return `Bearer ${localStorage.getItem("token")}`
 }
