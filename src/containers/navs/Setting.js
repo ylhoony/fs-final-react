2626fcede155e966a1b4e0a90ca778f5c5650430
Hoon Lee <yl.hoony@gmail.com>
2018-07-23 16:48:39 -0700

    added structure to setting page

--- src/containers/navs/Setting.js
+++ src/containers/navs/Setting.js
@@ -1,11 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Card, Header, Icon, Segment } from "semantic-ui-react";
+
+import BreadcrumbDisplay from "../../components/BreadcrumbDisplay";
 
 class Setting extends Component {
-  render () {
+  render() {
+    const description = [
+      "Amy is a violinist with 2 years experience in the wedding industry.",
+      "She enjoys the outdoors and currently resides in upstate New York."
+    ].join(" ");
+
     return (
-      <h2>This is Setting Page.</h2>
-    )
+      <React.Fragment>
+        <main>
+          <Segment.Group>
+            <BreadcrumbDisplay breadcrumbList={["Setting"]} />
+
+            {/* Company Address and Contacts Section */}
+            <Segment>
+              <Segment.Group>
+                <Segment>
+                  <Header as="h3">Company Information</Header>
+                </Segment>
+                <Segment>
+                  <Card.Group centered>
+                    <Card>
+                      <Card.Content header="Company Addresses" />
+                      <Card.Content description={description} />
+                      <Card.Content extra>
+                        <Icon name="user" />
+                        4 Friends
+                      </Card.Content>
+                    </Card>
+
+                    <Card>
+                      <Card.Content header="Company Contacts" />
+                      <Card.Content description={description} />
+                      <Card.Content extra>
+                        <Icon name="user" />
+                        4 Friends
+                      </Card.Content>
+                    </Card>
+                  </Card.Group>
+                </Segment>
+              </Segment.Group>
+            </Segment>
+
+            {/*  */}
+            <Segment>
+              <Segment.Group>
+                <Segment>
+                  <Header as="h3">Financial?</Header>
+                </Segment>
+                <Segment>
+                  <Card.Group centered>
+                    <Card>
+                      <Card.Content as={Link} to="/payment-terms" header="Payment Terms" />
+                      <Card.Content description="this is description" />
+                      <Card.Content extra>
+                        <Icon name="user" />
+                        4 Friends
+                      </Card.Content>
+                    </Card>
+
+                    <Card>
+                      <Card.Content header="use this" />
+                      <Card.Content description="This is for other link" />
+                      <Card.Content extra>
+                        <Icon name="user" />
+                        4 Friends
+                      </Card.Content>
+                    </Card>
+                  </Card.Group>
+                </Segment>
+              </Segment.Group>
+            </Segment>
+
+          </Segment.Group>
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
-export default Setting;
+export default withRouter(Setting);
