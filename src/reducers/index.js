85b8210ccf23bb4ff8bfa42d4a719b9088708345
Hoon Lee <yl.hoony@gmail.com>
2018-07-23 16:47:57 -0700

    added paymentTerms reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,17 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import { accountsReducer } from './accountsReducer';
-import { countriesReducer } from './countriesReducer';
-import { currenciesReducer } from './currenciesReducer';
-import { paymentOptionsReducer } from './paymentOptionsReducer';
-import { userReducer } from './userReducer';
+import { accountsReducer } from "./accountsReducer";
+import { countriesReducer } from "./countriesReducer";
+import { currenciesReducer } from "./currenciesReducer";
+import { paymentOptionsReducer } from "./paymentOptionsReducer";
+import { paymentTermsReducer } from "./paymentTermsReducer";
+import { userReducer } from "./userReducer";
 
 export const rootReducer = combineReducers({
   accounts: accountsReducer,
   countries: countriesReducer,
   currencies: currenciesReducer,
   paymentOptions: paymentOptionsReducer,
+  paymentTerms: paymentTermsReducer,
   user: userReducer,
-})
+});
