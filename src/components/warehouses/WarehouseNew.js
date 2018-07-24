2d5d29ab1b687cd13a3ca97f4157ea3658a97546
Hoon Lee <yl.hoony@gmail.com>
2018-07-24 12:52:17 -0700

    created file, and set basic contents

---
+++ src/components/warehouses/WarehouseNew.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class WarehouseNew extends Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default WarehouseNew;
+
+  // account_id,
+  // name,
+  // contact,
+  // street1,
+  // street2,
+  // city,
+  // state,
+  // country_id,
+  // postal_code,
+  // email,
+  // phone,
+  // fax,
+  // active,
