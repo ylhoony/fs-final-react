99597c981abed16ebe0a328184af148614962f72
Hoon Lee <yl.hoony@gmail.com>
2018-07-24 12:47:24 -0700

    created file and added getWarehouses & createWarehouse actions

---
+++ src/actions/warehousesActions.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import types from "./types";
+
+export const getWarehouses = params => dispatch => {
+  dispatch({ type: "GET_WAREHOUSES_BEGIN" });
+  axios
+    .get("/api/v1/warehouses", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json"
+      },
+      params: params
+    })
+    .then(res =>
+      dispatch({ type: "GET_WAREHOUSES_SUCCESS", payload: res.data })
+    );
+};
+
+export const createWarehouse = (data) => {
+
+}
