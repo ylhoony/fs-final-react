06cc96e79587ba6678dc927c02fdfe3f4a643dbd
Hoon Lee <yl.hoony@gmail.com>
2018-07-24 12:50:11 -0700

    commented out unused line

--- src/actions/warehousesActions.js
+++ src/actions/warehousesActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import types from "./types";
+// import types from "./types";
 
 export const getWarehouses = params => dispatch => {
   dispatch({ type: "GET_WAREHOUSES_BEGIN" });
