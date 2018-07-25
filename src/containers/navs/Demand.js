99b212a8e8cfcf41ed12c653f7f8926f4a95a946
Hoon Lee <yl.hoony@gmail.com>
2018-07-25 16:31:24 -0700

    added basic structure

--- src/containers/navs/Demand.js
+++ src/containers/navs/Demand.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Card, Header, Icon, Segment } from "semantic-ui-react";
+
+import BreadcrumbDisplay from "../../components/BreadcrumbDisplay";
 
 class Demand extends Component {
-  render () {
+  render() {
     return (
-      <h2>This is Demand Page.</h2>
-    )
+      <React.Fragment>
+        <main>
+          <Segment.Group>
+            <BreadcrumbDisplay breadcrumbList={["Demand"]} />
+
+            <Segment>
+              
+            </Segment>
+          </Segment.Group>
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
