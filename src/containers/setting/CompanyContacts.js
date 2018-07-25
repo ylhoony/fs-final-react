84e3be007a3e258f1c76ca07492d89fb0b225eeb
Hoon Lee <yl.hoony@gmail.com>
2018-07-25 16:40:49 -0700

    created file and adde basic structure

---
+++ src/containers/setting/CompanyContacts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CompanyContacts extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main>
+          this is CompanyContacts page.
+        </main>
+      </React.Fragment>
+    )
+  }
+}
+
+export default CompanyContacts;
