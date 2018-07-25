b83f042933fe0e2a6a3cd95a0380b7121a3e10ab
Hoon Lee <yl.hoony@gmail.com>
2018-07-24 18:24:08 -0700

    added createWarehouse, getWarehouse, updateWarehouse actions

--- src/actions/warehousesActions.js
+++ src/actions/warehousesActions.js
@@ -16,6 +16,49 @@ export const getWarehouses = params => dispatch => {
     );
 };
 
-export const createWarehouse = (data) => {
+export const createWarehouse = (data, params) => dispatch => {
+  dispatch({ type: "CREATE_WAREHOUSE_BEGIN" });
+  axios({
+    method: "POST",
+    url: "/api/v1/warehouses",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json"
+    },
+    data: data,
+    params: params
+  }).then(res =>
+    dispatch({ type: "CREATE_WAREHOUSE_SUCCESS", payload: res.data })
+  );
+};
 
-}
+export const getWarehouse = (warehouseId, params) => dispatch => {
+  dispatch({ type: "GET_WAREHOUSE_BEGIN" });
+  axios
+    .get(`/api/v1/warehouses/${warehouseId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json"
+      },
+      params: params
+    })
+    .then(res =>
+      dispatch({ type: "GET_WAREHOUSE_SUCCESS", payload: res.data })
+    );
+};
+
+export const updateWarehouse = (warehouseId, data, params) => dispatch => {
+  dispatch({ type: "UPDATE_WAREHOUSE_BEGIN" });
+  axios({
+    method: "PUT",
+    url: `/api/v1/warehouses/${warehouseId}`,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json"
+    },
+    data: data,
+    params: params
+  }).then(res =>
+    dispatch({ type: "UPDATE_WAREHOUSE_SUCCESS", payload: res.data })
+  );
+};
