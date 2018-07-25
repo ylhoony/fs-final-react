1b3266ddb919edac52ec22b7817917be3b79b3ee
Hoon Lee <yl.hoony@gmail.com>
2018-07-25 16:32:15 -0700

    created Customers container file, and set basic structure

---
+++ src/containers/demand/Customers.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Customers extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main>
+          customers page!
+        </main>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Customers;
