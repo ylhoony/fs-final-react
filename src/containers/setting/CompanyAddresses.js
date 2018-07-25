aa7aa6dde3a41a96f787f5ea5c20ab85c5a1d821
Hoon Lee <yl.hoony@gmail.com>
2018-07-25 16:32:50 -0700

    created company address file and set basic structure.

---
+++ src/containers/setting/CompanyAddresses.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CompanyAddresses extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main>
+          this is CompanyAddresses page.
+        </main>
+      </React.Fragment>
+    )
+  }
+}
+
+export default CompanyAddresses;
