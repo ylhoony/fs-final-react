a0695dbf5909bb34028ef3c236b76adaf486b2f1
Hoon Lee <yl.hoony@gmail.com>
2018-07-26 11:23:20 -0700

    added accountAddresses Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,9 +7,11 @@ import { paymentOptionsReducer } from "./paymentOptionsReducer";
 import { paymentTermsReducer } from "./paymentTermsReducer";
 import { userReducer } from "./userReducer";
 import { warehousesReducer } from "./warehousesReducer";
+import { accountAddressesReducer } from "./accountAddressesReducer";
 
 export const rootReducer = combineReducers({
   accounts: accountsReducer,
+  accountAddresses: accountAddressesReducer,
   countries: countriesReducer,
   currencies: currenciesReducer,
   paymentOptions: paymentOptionsReducer,
