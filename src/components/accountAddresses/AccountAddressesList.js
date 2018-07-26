7fdb657b8aa8a32d877e8559a08211d0c83e4144
Hoon Lee <yl.hoony@gmail.com>
2018-07-25 18:01:07 -0700

    created AccountAddressesList file and set basic structure

---
+++ src/components/accountAddresses/AccountAddressesList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AccountAddressesList extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        this is Account Addresses List
+      </React.Fragment>
+    )
+  }
+}
+
+export default AccountAddressesList;
