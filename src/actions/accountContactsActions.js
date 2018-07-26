5436c52da1d28b4d302f231d50074ebc4425d7da
Hoon Lee <yl.hoony@gmail.com>
2018-07-26 15:15:43 -0700

    copied accountAddressesActions, and updated

---
+++ src/actions/accountContactsActions.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+// import types from "./types";
+
+export const getAccountContacts = params => dispatch => {
+  dispatch({ type: "GET_ACCOUNT_CONTACTS_BEGIN" });
+  axios
+    .get("/api/v1/account_contacts", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json"
+      },
+      params: params
+    })
+    .then(res =>
+      dispatch({ type: "GET_ACCOUNT_CONTACTS_SUCCESS", payload: res.data })
+    );
+};
+
+export const createAccountContact = (data, params) => dispatch => {
+  dispatch({ type: "CREATE_ACCOUNT_CONTACT_BEGIN" });
+  axios({
+    method: "POST",
+    url: "/api/v1/account_contacts",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json"
+    },
+    data: data,
+    params: params
+  }).then(res =>
+    dispatch({ type: "CREATE_ACCOUNT_CONTACT_SUCCESS", payload: res.data })
+  );
+};
+
+export const getAccountContact = (accountContactId, params) => dispatch => {
+  dispatch({ type: "GET_ACCOUNT_CONTACT_BEGIN" });
+  axios
+    .get(`/api/v1/account_contacts/${accountContactId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json"
+      },
+      params: params
+    })
+    .then(res =>
+      dispatch({ type: "GET_ACCOUNT_CONTACT_SUCCESS", payload: res.data })
+    );
+};
+
+export const updateAccountContact = (accountContactId, data, params) => dispatch => {
+  dispatch({ type: "UPDATE_ACCOUNT_CONTACT_BEGIN" });
+  axios({
+    method: "PUT",
+    url: `/api/v1/account_contacts/${accountContactId}`,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json"
+    },
+    data: data,
+    params: params
+  }).then(res =>
+    dispatch({ type: "UPDATE_ACCOUNT_CONTACT_SUCCESS", payload: res.data })
+  );
+};
