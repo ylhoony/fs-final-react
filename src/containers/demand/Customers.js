8fd2781c6fde1084c0cd03dd7738938be381802d
Hoon Lee <yl.hoony@gmail.com>
2018-07-27 10:17:17 -0700

    set customers routes

--- src/containers/demand/Customers.js
+++ src/containers/demand/Customers.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+import CustomersList from "../../components/customers/CustomersList";
+import CustomerForm from "../../components/customers/CustomerForm";
 
 class Customers extends Component {
   render() {
+    const { match } = this.props;
+
     return (
       <React.Fragment>
-        <main>
-          customers page!
-        </main>
+        <Switch>
+          <Route
+            exact
+            path={`${match.url}`}
+            component={() => <CustomersList />}
+          />
+          <Route
+            exact
+            path={`${match.url}/new`}
+            component={() => <CustomerForm />}
+          />
+          <Route
+            exact
+            path={`${match.url}/:customerId`}
+            component={() => <CustomerForm />}
+          />
+        </Switch>
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default Customers;
+export default withRouter(connect()(Customers));
