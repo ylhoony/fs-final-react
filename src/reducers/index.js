cdaed90f865aa056921c378e2d82d72bf30f023f
Hoon Lee <yl.hoony@gmail.com>
2018-07-27 10:17:00 -0700

    added import lines for customers reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers } from "redux";
 import { accountsReducer } from "./accountsReducer";
 import { countriesReducer } from "./countriesReducer";
 import { currenciesReducer } from "./currenciesReducer";
+import { customersReducer } from "./customersReducer";
 import { paymentOptionsReducer } from "./paymentOptionsReducer";
 import { paymentTermsReducer } from "./paymentTermsReducer";
 import { userReducer } from "./userReducer";
@@ -16,6 +17,7 @@ export const rootReducer = combineReducers({
   accountContacts: accountContactsReducer,
   countries: countriesReducer,
   currencies: currenciesReducer,
+  customers: customersReducer,
   paymentOptions: paymentOptionsReducer,
   paymentTerms: paymentTermsReducer,
   user: userReducer,
