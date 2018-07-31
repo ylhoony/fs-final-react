39d9ca920d8b66a539074f4b817a7b70c60c88f4
Hoon Lee <yl.hoony@gmail.com>
2018-07-30 17:45:40 -0700

    created file with basic structure

---
+++ src/components/companyDetails/ContactsTab.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ContactsTab extends Component {
+  render() {
+    return(
+      <React.Fragment>
+        <div>
+          ContactsTab page
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default ContactsTab;
