16379f6caefdae3675e6afd37beeeb4d5b7cd1ea
Hoon Lee <yl.hoony@gmail.com>
2018-08-01 15:22:45 -0700

    udpated supply page adding card

--- src/containers/navs/Supply.js
+++ src/containers/navs/Supply.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
+import { Card, Segment } from "semantic-ui-react";
+
+import BreadcrumbDisplay from "../../components/BreadcrumbDisplay";
 
 class Supply extends Component {
-  render () {
+  render() {
     return (
-      <h2>This is Supply Page.</h2>
-    )
+      <React.Fragment>
+        <main>
+          <Segment.Group>
+            <BreadcrumbDisplay
+              breadcrumbList={[{ name: "Supply", url: "/supply" }]}
+            />
+
+            <Segment>
+              <Card.Group className="">
+                <Card>
+                  <Card.Content as={Link} to="/suppliers" header="Suppliers" />
+                  <Card.Content description="Manage Suppliers" />
+                </Card>
+
+                <Card>
+                  <Card.Content
+                    as={Link}
+                    to="/suppliers"
+                    header="Purchase Orders"
+                    />
+                  <Card.Content description="purchase orders" />
+                </Card>
+              </Card.Group>
+            </Segment>
+          </Segment.Group>
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
-export default Supply;
+export default withRouter(connect()(Supply));
