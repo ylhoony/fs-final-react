ba88199140b650c0025ddc6da5e2b5bd3ec3f1ff
Hoon Lee <yl.hoony@gmail.com>
2018-07-31 18:36:37 -0700

    added fields for addresses

--- src/components/companyDetails/AddressesTab.js
+++ src/components/companyDetails/AddressesTab.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
-import { Button, Header, Icon, Image, Item, Table } from "semantic-ui-react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { Button, Form, Icon, Table } from "semantic-ui-react";
+
+import { buildCountriesOptions } from "../../helpers/optionsBuilder";
 
 class AddressesTab extends Component {
   render() {
-    console.log(this.props);
+    const { addresses, countries } = this.props;
+    const countriesOptions = buildCountriesOptions(countries);
 
     return (
       <React.Fragment>
@@ -22,26 +27,136 @@ class AddressesTab extends Component {
               <Table.HeaderCell>Phone</Table.HeaderCell>
               <Table.HeaderCell>Fax</Table.HeaderCell>
               <Table.HeaderCell>Status</Table.HeaderCell>
+              <Table.HeaderCell />
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            {this.props.addresses.map((address, index) => {
+            {addresses.map((address, index) => {
               return (
                 <Table.Row key={index}>
-                  <Table.Cell>{address.company_name}</Table.Cell>
-                  <Table.Cell>{address.contact}</Table.Cell>
-                  <Table.Cell>{address.street1}</Table.Cell>
-                  <Table.Cell>{address.street2}</Table.Cell>
-                  <Table.Cell>{address.city}</Table.Cell>
-                  <Table.Cell>{address.state}</Table.Cell>
-                  <Table.Cell>{address.country}</Table.Cell>
-                  <Table.Cell>{address.postal_code}</Table.Cell>
-                  <Table.Cell>{address.email}</Table.Cell>
-                  <Table.Cell>{address.phone}</Table.Cell>
-                  <Table.Cell>{address.fax}</Table.Cell>
                   <Table.Cell>
-                    {address.active ? "active" : "inactive"}
+                    <Form.Input
+                      fluid
+                      name="company_name"
+                      placeholder="company name"
+                      value={address.company_name}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="contact"
+                      placeholder="contact"
+                      value={address.contact}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="street1"
+                      placeholder="street 1"
+                      value={address.street1}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="street2"
+                      placeholder="street2"
+                      value={address.street2}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="city"
+                      placeholder="city"
+                      value={address.city}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="state"
+                      placeholder="state"
+                      value={address.state}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Select
+                      name="country_id"
+                      options={countriesOptions}
+                      text={
+                        address.country_id
+                          ? countriesOptions.find(
+                              country => country.key === parseInt(address.country_id, 10)
+                            ).text
+                          : "Select country"
+                      }
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="postal_code"
+                      placeholder="postal_code"
+                      value={address.postal_code}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="email"
+                      placeholder="email"
+                      value={address.email}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="phone"
+                      placeholder="phone"
+                      value={address.phone}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Input
+                      fluid
+                      name="fax"
+                      placeholder="fax"
+                      value={address.fax}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Form.Checkbox
+                      toggle
+                      name="active"
+                      placeholder="active"
+                      checked={address.active}
+                      onChange={e => this.props.handleAddressChange(e, index)}
+                    />
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Button
+                      basic
+                      color="red"
+                      compact
+                      icon="minus"
+                      size="mini"
+                      onClick={e => this.props.handleRemoveAddress(e, index)}
+                    />
                   </Table.Cell>
                 </Table.Row>
               );
@@ -49,7 +164,7 @@ class AddressesTab extends Component {
           </Table.Body>
           <Table.Footer>
             <Table.Row>
-              <Table.Cell colSpan="8">
+              <Table.Cell colSpan="9">
                 <Button
                   basic
                   color="teal"
@@ -68,4 +183,8 @@ class AddressesTab extends Component {
   }
 }
 
-export default AddressesTab;
+const mapStateToProps = ({ countries }) => {
+  return { countries: countries.countries };
+};
+
+export default withRouter(connect(mapStateToProps)(AddressesTab));
