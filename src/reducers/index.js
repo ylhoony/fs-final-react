f263372790b9843634fa0f56a30136536032d2dd
Hoon Lee <yl.hoony@gmail.com>
2018-08-01 15:54:10 -0700

    added suppliersReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,16 @@
 import { combineReducers } from "redux";
 
 import { accountsReducer } from "./accountsReducer";
+import { accountAddressesReducer } from "./accountAddressesReducer";
+import { accountContactsReducer } from "./accountContactsReducer";
 import { countriesReducer } from "./countriesReducer";
 import { currenciesReducer } from "./currenciesReducer";
 import { customersReducer } from "./customersReducer";
 import { paymentOptionsReducer } from "./paymentOptionsReducer";
 import { paymentTermsReducer } from "./paymentTermsReducer";
+import { suppliersReducer } from "./suppliersReducer";
 import { userReducer } from "./userReducer";
 import { warehousesReducer } from "./warehousesReducer";
-import { accountAddressesReducer } from "./accountAddressesReducer";
-import { accountContactsReducer } from "./accountContactsReducer"
 
 export const rootReducer = combineReducers({
   accounts: accountsReducer,
@@ -20,6 +21,7 @@ export const rootReducer = combineReducers({
   customers: customersReducer,
   paymentOptions: paymentOptionsReducer,
   paymentTerms: paymentTermsReducer,
+  suppliers: suppliersReducer,
   user: userReducer,
   warehouses: warehousesReducer,
 });
