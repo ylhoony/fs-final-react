60360b6b164e19e5bc433643dd7ddea773e9dbe4
Hoon Lee <yl.hoony@gmail.com>
2018-08-01 18:11:37 -0700

    set product nav page with card

--- src/containers/navs/Product.js
+++ src/containers/navs/Product.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
+import { Card, Segment } from "semantic-ui-react";
+
+import BreadcrumbDisplay from "../../components/BreadcrumbDisplay";
 
 class Product extends Component {
-  render () {
+  render() {
     return (
-      <h2>This is Product Page.</h2>
-    )
+      <React.Fragment>
+        <main>
+          <Segment.Group>
+            <BreadcrumbDisplay
+              breadcrumbList={[{ name: "Product", url: "/product" }]}
+            />
+
+            <Segment>
+              <Card.Group className="">
+                <Card>
+                  <Card.Content as={Link} to="/products" header="Products" />
+                  <Card.Content description="Manage Products" />
+                </Card>
+
+                <Card>
+                  <Card.Content
+                    as={Link}
+                    to="/products"
+                    header="Product Family"
+                  />
+                  <Card.Content description="sales orders" />
+                </Card>
+              </Card.Group>
+            </Segment>
+          </Segment.Group>
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
-export default Product;
+export default withRouter(connect()(Product));
