85c840c3cc709acb16c1de90ac1cbc6ecc76a568
Hoon Lee <yl.hoony@gmail.com>
2018-08-02 15:54:46 -0700

    created ProductBrands container file

---
+++ src/containers/setting/ProductBrands.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class ProductBrands extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          ProductBrands page
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default ProductBrands;
