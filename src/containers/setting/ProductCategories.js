097e7d740673a8612e8fabc5179bc47e566347b9
Hoon Lee <yl.hoony@gmail.com>
2018-08-02 15:55:01 -0700

    created Product Categories container file

---
+++ src/containers/setting/ProductCategories.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class ProductCategories extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          ProductCategories page
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default ProductCategories;
