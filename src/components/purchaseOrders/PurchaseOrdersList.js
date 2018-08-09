dd93dbe77bfee80853eedfd28cf11ec9642dfd88
Hoon Lee <yl.hoony@gmail.com>
2018-08-09 15:51:24 -0700

    connected to redux, and set constructor

--- src/components/purchaseOrders/PurchaseOrdersList.js
+++ src/components/purchaseOrders/PurchaseOrdersList.js
@@ -1,13 +1,65 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Button, Header, Icon, Segment, Table } from "semantic-ui-react";
+
+import { actions } from "../../actions/index";
+
+import BreadcrumbDisplay from "../BreadcrumbDisplay";
+import Loading from "../Loading";
 
 class PurchaseOrdersList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      purchase_order: {}
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
+    const { currentAccountLoading } = this.props;
+
     return (
       <React.Fragment>
-        Purchase Orders List 
+        <main>
+          <Segment.Group>
+            <BreadcrumbDisplay
+              breadcrumbList={[
+                { name: "Demand", url: "/demand" },
+                { name: "Customers", url: "/customers" }
+              ]}
+            />
+
+            <Segment>hello</Segment>
+          </Segment.Group>
+        </main>
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default PurchaseOrdersList;
+const mapStateToProps = ({ user }) => {
+  return {
+    currentAccount: user.currentAccount,
+    currentAccountLoading: user.currentAccountLoading
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(PurchaseOrdersList)
+);
