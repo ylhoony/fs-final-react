81e272e4d24eacac14bc26a971f2f308ffac251f
Hoon Lee <yl.hoony@gmail.com>
2018-08-14 17:45:21 -0700

    added PurchaseOrdersListRows and updated onClick

--- src/components/purchaseOrders/PurchaseOrdersListRows.js
+++ src/components/purchaseOrders/PurchaseOrdersListRows.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Table } from "semantic-ui-react"
 
-const PurchaseOrdersListRows = ({ purchaseOrders }) => {
+const PurchaseOrdersListRows = ({ purchaseOrders, handleTableCellClick }) => {
   if (!purchaseOrders.length) {
     return (
       <Table.Row>
@@ -12,20 +12,20 @@ const PurchaseOrdersListRows = ({ purchaseOrders }) => {
     return purchaseOrders.map(po => {
       return (
         <Table.Row key={po.id} data-id={po.id}>
-          <Table.Cell onClick={this.handleTableCellClick}>{po.id}</Table.Cell>
-          <Table.Cell onClick={this.handleTableCellClick}>
+          <Table.Cell onClick={handleTableCellClick}>{po.id}</Table.Cell>
+          <Table.Cell onClick={handleTableCellClick}>
             {po.order_reference}
           </Table.Cell>
-          <Table.Cell onClick={this.handleTableCellClick}>
+          <Table.Cell onClick={handleTableCellClick}>
             {po.supplier.name}
           </Table.Cell>
-          <Table.Cell onClick={this.handleTableCellClick}>
-            {po.shipping_address.name}
+          <Table.Cell onClick={handleTableCellClick}>
+            {po.shipping_address.company_name}
           </Table.Cell>
-          <Table.Cell onClick={this.handleTableCellClick}>
+          <Table.Cell onClick={handleTableCellClick}>
             {po.warehouse.name}
           </Table.Cell>
-          <Table.Cell onClick={this.handleTableCellClick}>Amount</Table.Cell>
+          <Table.Cell onClick={handleTableCellClick}>Amount</Table.Cell>
           <Table.Cell>Status</Table.Cell>
         </Table.Row>
       );
