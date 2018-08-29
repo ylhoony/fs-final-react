9195ef16a8785b71f7150c8c83c1e891d8014e9b
Hoon Lee <yl.hoony@gmail.com>
2018-08-29 14:45:31 -0700

    added key to map function

--- src/components/header/NavMenu.js
+++ src/components/header/NavMenu.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 
 const NavMenu = ({ list }) => {
-  return list.map(menu => {
-    return <Menu.Item as={Link} to={menu.endpoint} name={menu.name} />;
+  return list.map((menu, index) => {
+    return (
+      <Menu.Item as={Link} key={index} to={menu.endpoint} name={menu.name} />
+    );
   });
 };
 
