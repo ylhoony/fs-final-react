1102057386b91c7bfcc25ce4081fa3b34e8ee4f2
Hoon Lee <yl.hoony@gmail.com>
2018-08-29 15:43:31 -0700

    updated signIn action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -18,9 +18,10 @@ export const signUp = data => async dispatch => {
 
 export const signIn = data => async dispatch => {
   dispatch({ type: 'SIGN_IN_USER_BEGIN' });
-  const res = await axios.post("/api/v1/sign_in", data);
-  localStorage.setItem("token", res.data.token);
-  dispatch({ type: 'SIGN_IN_USER_SUCCESS', payload: res.data.user });
+  return axios.post("/api/v1/sign_in", data).then(res => {
+    localStorage.setItem("token", res.data.token)
+    dispatch({ type: 'SIGN_IN_USER_SUCCESS', payload: res.data.user });
+  })
 };
 
 export const signOut = () => dispatch => {
